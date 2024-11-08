a2e6bb6bc52891428d82077f8a0e95ea4fec2941
Hugo-walando <hugowalando@gmail.com>
2024-11-08 17:31:32 +0100

    import magic ui grid pattern & import font

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,19 @@
+'use client';
+
+import GridPattern from '@/components/ui/grid-pattern';
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <GridPattern
+        width={100}
+        height={105}
+        x={-1}
+        y={-1}
+        className='absolute inset-0'
+      />
+      <section className='h-screen w-full bg-gradient-to-b to-landing_gradient_brown from-landing_gradient_blue '></section>
+      <section className='h-screen bg-white'></section>
+    </>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className='absolute'>Navbar</div>;
 };
 
 export default Navbar;
